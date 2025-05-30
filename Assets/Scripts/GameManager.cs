5a9447c107d863f53554eec1dfbb48192e696510
ReghanH <134046370+ReghanH@users.noreply.github.com>
2025-05-30 13:54:12 +1000

    -added my stuff
    - still unfinished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    //Pickup and Level Completion logic
+    public int currentPickups = 0;
+    public int maxPickups = 5;
+    public bool levelComplete = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LevelCompleteCheck(){
+
+    }
+
+}
