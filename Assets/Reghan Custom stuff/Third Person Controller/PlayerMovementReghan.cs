8f6fe93582908a737109b163328efd64c6d334ec
ReghanH <134046370+ReghanH@users.noreply.github.com>
2025-05-31 22:36:03 +1000

    movement script changed

--- Assets/Reghan Custom stuff/Third Person Controller/PlayerMovementReghan.cs
+++ Assets/Reghan Custom stuff/Third Person Controller/PlayerMovementReghan.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovementReghan : MonoBehaviour{
 
     public Animator anim;
+    public float rotSpeed = 145f;
+    public float moveSpeed = 1f;
+    public float sprintSpeed = 2f;
+    public float jumpHeight = 3f;
+    private Rigidbody rb;
 
-    public float rotSpeed = 10;
+    //private bool canJump = true;
+    //private float jumpCooldown = 2f;
+    //private float jumpTimer = 0f;
 
     // Start is called before the first frame update
     void Start(){
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        rb.angularDamping = 10f;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        anim.applyRootMotion = false;
     }
 
     // Update is called once per frame
@@ -25,17 +38,22 @@ public class PlayerMovementReghan : MonoBehaviour{
     }
 
     private void ForwardMovement(){
+        float moveDirectionZ = 0f;
         if(Input.GetKey("w")){
             anim.SetBool("Walking", true);
             if (Input.GetKey(KeyCode.LeftShift)){
                 anim.SetBool("Running", true);
+                moveDirectionZ = sprintSpeed;
             } else{
                 anim.SetBool("Running", false);
+                moveDirectionZ = moveSpeed;
             }
         } else if (Input.GetKeyUp("w")) {
             anim.SetBool("Walking", false);
             anim.SetBool("Running", false);
         }
+        Vector3 move = transform.forward * moveDirectionZ * Time.deltaTime;
+        rb.MovePosition(rb.position + move);
     }
 
     private void Turning(){
@@ -57,5 +75,27 @@ public class PlayerMovementReghan : MonoBehaviour{
         } else if(Input.GetKeyUp("e")){
             anim.SetBool("Waving", false);
         }
+    }
+
+    private bool knockedOver = false;
+    private void CheckIfKnockedOver()
+    {
+        float xRotation = Mathf.Abs(transform.rotation.eulerAngles.x);
+        float zRotation = Mathf.Abs(transform.rotation.eulerAngles.z);
+
+        if ((xRotation > 80f || zRotation > 80f) && !knockedOver)
+        {
+            knockedOver = true;
+            ResetRotation();
+        }
+        else if (knockedOver)
+        {
+            knockedOver = false;
+        }
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
     }
 }
