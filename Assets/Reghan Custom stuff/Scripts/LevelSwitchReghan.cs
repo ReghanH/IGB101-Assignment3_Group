8f857e6ab608d15485215ca67e1c76b5d59e13de
ReghanH <134046370+ReghanH@users.noreply.github.com>
2025-05-31 22:17:46 +1000

    green cube

---
+++ Assets/Reghan Custom stuff/Scripts/LevelSwitchReghan.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitchReghan : MonoBehaviour
+{
+    GameManagerReghan gameManager;
+    public string nextLevel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManagerReghan").GetComponent<GameManagerReghan>();
+    }
+
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        if(otherObject.transform.tag == "PlayerReghan")
+        {
+            if (gameManager.levelComplete)
+            {
+                SceneManager.LoadScene("Acelin_Main");
+            }
+        }
+    }
+}
